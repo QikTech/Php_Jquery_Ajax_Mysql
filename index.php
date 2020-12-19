6611bb702dfafee19c72307162c8f85340d5992d
Prasad sawant <prassaw@gmail.com>
2020-12-19 20:59:18 +0530

    Login Form Begins

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+     <!-- UIkit CSS -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.1/dist/css/uikit.min.css" />
+</head>
+<body>
+
+     <!-- <div class="uk-section uk-contailer uk-text-center"> -->
+     <!-- <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+     <?php echo "Qiktech in the house"; ?>
+     </div> -->
+     <!-- <div class="uk-section uk-contailer uk-text-center"> -->
+     <div class="uk-align-center uk-grid uk-width-1-3@m uk-width-1-1" uk-grid="">
+     <form class="uk-form-stacked" js-login>
+
+          <div class="uk-margin">
+               <label class="uk-form-label" for="form-stacked-text">Email</label>
+               <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+               </div>
+          </div>
+          <div class="uk-margin">
+               <label class="uk-form-label" for="form-stacked-text">Password</label>
+               <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="enter password">
+               </div>
+          </div>
+          <div class="uk-margin">
+               <button class="uk-button uk-button-default" type="submit">Login</button>
+          </div>
+
+              
+
+     </form>
+     </div>
+          
+     </div>
+
+          <!-- jQuery -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+          <!-- UIkit JS -->
+     <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.1/dist/js/uikit.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.1/dist/js/uikit-icons.min.js"></script>
+
+    
+
+</body>
+</html>
